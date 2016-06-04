d4e833a55d8cbc3ef09cf196081b87b0034f19b9
Lookat2 <ajay1976chaudhary@rediffmail.com>
2016-06-04 11:50:42 +0530

    Add files via upload

---
+++ ajay/bhasha/migrations/language/en.js
@@ -0,0 +1,20 @@
+exports.english = {
+    "Independent_vowels": { // -()
+    // Independent vowels
+    },
+
+    "Dependent_vowel": { // +()
+    },
+
+    "Consonants": { // ()+
+    },
+
+    "Accent_marks": {
+    },
+
+    "Others": { // ()
+    },
+
+    // Virama
+    "VIRAMA"    :   ""
+};
