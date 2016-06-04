054d7564ae1f87aad878b5596f748a64943c854d
Lookat2 <ajay1976chaudhary@rediffmail.com>
2016-06-04 11:52:10 +0530

    Add files via upload

---
+++ ajay/bhasha/migrations/language/iso2itrans.js
@@ -0,0 +1,80 @@
+// ISO 15919
+exports.iso2itrans = {
+    // http://en.wikipedia.org/wiki/Wikipedia:Indic_transliteration
+    // http://homepage.ntlworld.com/stone-catend/trimain1.htm
+    // http://www.virtualvinodh.com/wp/character-matrix/
+
+    "ā"     :   "A",
+    //"æ"     :   "",
+    //"ǣ"     :   "",
+    "ī"     :   "I",
+    "ū"     :   "U",
+    "r̥"     :   "RRi",
+    "r̥̄"     :   "RRI",
+    "l̥"     :   "LLi",
+    "l̥̄"     :   "LLI",
+    "ĕ"     :   "a.c",  // need to check again
+    "ē"     :   "E",
+    "ŏ"     :   "A.c",  // need to check again
+    "ō"     :   "O",
+    "m̐"     :   ".N",
+    "n̆"     :   ".N",   // for Telugu
+    "ṃ"     :   "M",    // *** not correct for Gurmukhi ***
+    "ṁ"     :   "M",
+    "ḥ"     :   "H",
+    "'"     :   ".a",
+    "ṅ"     :   "~N",
+    "c"     :   "ch",   // will also handle ch -> chh/Ch
+    "ñ"     :   "~n",
+    "ṭ"     :   "T",    // will also handle ṭh -> Th
+    "ḍ"     :   "D",    // will also handle ḍh -> Dh
+    "ṇ"     :   "N",
+    "ṉ"     :   "^n",
+    "ṟ"     :   "R",
+    //"r̆"     :   "",
+    "ḷ"     :   "L",
+    //"ḻ"     :   "",
+    "ś"     :   "sh",
+    "ṣ"     :   "Sh",
+    "q"     :   "ḳ",
+    "ḵẖ"    :   "kḥ",
+    "k͟h"    :   "kḥ",
+    "ġ"     :   "g̣",
+    "z"     :   "j̣",
+    "ṛ"     :   "Ḍ",
+    "ṛh"    :   "Dḥ",
+    "f"     :   "pḥ",
+    "ẏ"     :   "ỵ",
+    "t̤"     :   "ṭ",
+    "s̤"     :   "ṣ",
+    "h̤"     :   "ḥ",
+    "w"     :   "ṿ",
+    "ṯ"     :   "t"
+};
+
+exports.iso2itrans = {
+    // http://en.wikipedia.org/wiki/International_Alphabet_of_Sanskrit_Transliteration
+    // http://en.wikipedia.org/wiki/Devanagari_transliteration
+    // http://www.aczoom.com/itrans/html/romancsx/node4.html
+
+    "ā"     :   "A",
+    "ī"     :   "I",
+    "ū"     :   "U",
+    "ṛ"     :   "RRi",
+    "ṝ"     :   "RRI",
+    "ḷ"     :   "LLi",
+    "ḹ"     :   "LLI",
+    "e"     :   "E",
+    "o"     :   "O",
+    "ṃ"     :   "M",
+    "ḥ"     :   "H",
+    "'"     :   ".a",
+    "c"     :   "ch",   // will also handle ch -> chh/Ch
+    "ṭ"     :   "T",    // will also handle ṭh -> Th
+    "ḍ"     :   "D",    // will also handle ḍh -> Dh
+    "ṅ"     :   "~N",
+    "ñ"     :   "~n",
+    "ṇ"     :   "N",
+    "ś"     :   "sh",
+    "ṣ"     :   "Sh"
+};
