2a201d8d4c0a7c52ea5d1c5181c93a59d8335af3
Lookat2 <ajay1976chaudhary@rediffmail.com>
2016-06-04 11:50:11 +0530

    Add files via upload

---
+++ ajay/bhasha/migrations/language/devanagari.js
@@ -0,0 +1,231 @@
+exports.devanagari = {
+    "Independent_vowels": { // -()
+        // Independent vowels
+        //""    :   "\u0904", // ऄ DEVANAGARI LETTER SHORT A
+        "a"     :   "\u0905", // अ DEVANAGARI LETTER A
+        "A"     :   "\u0906", // आ DEVANAGARI LETTER AA
+        "aa"    :   "\u0906", // आ DEVANAGARI LETTER AA
+        "i"     :   "\u0907", // इ DEVANAGARI LETTER I
+        "I"     :   "\u0908", // ई DEVANAGARI LETTER II
+        "ii"    :   "\u0908", // ई DEVANAGARI LETTER II
+        "u"     :   "\u0909", // उ DEVANAGARI LETTER U
+        "U"     :   "\u090A", // ऊ DEVANAGARI LETTER UU
+        "uu"    :   "\u090A", // ऊ DEVANAGARI LETTER UU
+        "R^i"   :   "\u090B", // ऋ DEVANAGARI LETTER VOCALIC R
+        "RRi"   :   "\u090B", // ऋ DEVANAGARI LETTER VOCALIC R
+        "L^i"   :   "\u090C", // ऌ DEVANAGARI LETTER VOCALIC L
+        "LLi"   :   "\u090C", // ऌ DEVANAGARI LETTER VOCALIC L
+        "a.c"   :   "\u090D", // ऍ DEVANAGARI LETTER CANDRA E ** Manual Change ***
+        "e"     :   "\u090E", // ऎ DEVANAGARI LETTER SHORT E • for transcribing Dravidian short e   ** Manual Change ***
+        "E"     :   "\u090F", // ए DEVANAGARI LETTER E
+        "ee"    :   "\u090F", // ए DEVANAGARI LETTER E
+        "ai"    :   "\u0910", // ऐ DEVANAGARI LETTER AI
+        "A.c"   :   "\u0911", // ऑ DEVANAGARI LETTER CANDRA O ** Manual Change ***
+        "aa.c"  :   "\u0911", // ऑ DEVANAGARI LETTER CANDRA O ** Manual Change ***
+        "o"     :   "\u0912", // ऒ DEVANAGARI LETTER SHORT O • for transcribing Dravidian short o   *** Manual Change ***
+        "O"     :   "\u0913", // ओ DEVANAGARI LETTER O
+        "oo"    :   "\u0913", // ओ DEVANAGARI LETTER O
+        "au"    :   "\u0914", // औ DEVANAGARI LETTER AU
+
+
+        // Independent vowel for Marathi
+        //""    :   "\u0972", // ॲ DEVANAGARI LETTER CANDRA A
+
+        // Independent vowels for Kashmiri
+        //""    :   "\u0973", // ॳ DEVANAGARI LETTER OE
+        //""    :   "\u0974", // ॴ DEVANAGARI LETTER OOE
+        //""    :   "\u0975", // ॵ DEVANAGARI LETTER AW
+        //""    :   "\u0976", // ॶ DEVANAGARI LETTER UE
+        //""    :   "\u0977", // ॷ DEVANAGARI LETTER UUE
+
+        // Additional vowels for Sanskrit
+        "R^I"   :   "\u0960", // ॠ DEVANAGARI LETTER VOCALIC RR
+        "RRI"   :   "\u0960", // ॠ DEVANAGARI LETTER VOCALIC RR
+        "L^I"   :   "\u0961", // ॡ DEVANAGARI LETTER VOCALIC LL
+        "LLI"   :   "\u0961", // ॡ DEVANAGARI LETTER VOCALIC LL
+
+        "OM"    :   "\u0950", // ॐ DEVANAGARI OM
+        "ooM"   :   "\u0950", // ॐ DEVANAGARI OM
+        "AUM"   :   "\u0950"  // ॐ DEVANAGARI OM
+    },
+
+    "Dependent_vowel": { // +()
+        // Dependent vowel signs for Kashmiri
+        //""    :   "\u093A", // $ऺ DEVANAGARI VOWEL SIGN OE
+        //""    :   "\u093B", // $ऻ DEVANAGARI VOWEL SIGN OOE
+
+        // Dependent vowel signs
+        "a"     :   "",       // ADDED
+        "A"     :   "\u093E", // $ा DEVANAGARI VOWEL SIGN AA
+        "aa"    :   "\u093E", // $ा DEVANAGARI VOWEL SIGN AA
+        "i"     :   "\u093F", // $ि DEVANAGARI VOWEL SIGN I • stands to the left of the consonant
+        "I"     :   "\u0940", // $ी DEVANAGARI VOWEL SIGN II
+        "ii"    :   "\u0940", // $ी DEVANAGARI VOWEL SIGN II
+        "u"     :   "\u0941", // $ु DEVANAGARI VOWEL SIGN U
+        "U"     :   "\u0942", // $ू DEVANAGARI VOWEL SIGN UU
+        "uu"    :   "\u0942", // $ू DEVANAGARI VOWEL SIGN UU
+        "R^i"   :   "\u0943", // $ृ DEVANAGARI VOWEL SIGN VOCALIC R
+        "RRi"   :   "\u0943", // $ृ DEVANAGARI VOWEL SIGN VOCALIC R
+        "R^I"   :   "\u0944", // $ॄ DEVANAGARI VOWEL SIGN VOCALIC RR
+        "RRI"   :   "\u0944", // $ॄ DEVANAGARI VOWEL SIGN VOCALIC RR
+        "a.c"   :   "\u0945", // $ॅ DEVANAGARI VOWEL SIGN CANDRA E = candra ** Manual Change ***
+        "e"     :   "\u0946", // $ॆ DEVANAGARI VOWEL SIGN SHORT E • for transcribing Dravidian vowels   ** Manual Change ***
+        "E"     :   "\u0947", // $े DEVANAGARI VOWEL SIGN E
+        "ee"    :   "\u0947", // $े DEVANAGARI VOWEL SIGN E
+        "ai"    :   "\u0948", // $ै DEVANAGARI VOWEL SIGN AI
+        "A.c"   :   "\u0949", // $ॉ DEVANAGARI VOWEL SIGN CANDRA O ** Manual Change ***
+        "aa.c"  :   "\u0949", // $ॉ DEVANAGARI VOWEL SIGN CANDRA O ** Manual Change ***
+        "o"     :   "\u094A", // $ॊ DEVANAGARI VOWEL SIGN SHORT O • for transcribing Dravidian vowels    ** Manual Change ***
+        "O"     :   "\u094B", // $ो DEVANAGARI VOWEL SIGN O
+        "oo"    :   "\u094B", // $ो DEVANAGARI VOWEL SIGN O
+        "au"    :   "\u094C", // $ौ DEVANAGARI VOWEL SIGN AU
+
+        // Dependent vowel signs
+        //""    :   "\u094E", // $ॎ DEVANAGARI VOWEL SIGN PRISHTHAMATRA E • character has historic use only • combines with E to form AI, with AA to form O, and with O to form AU
+        //""    :   "\u094F", // $ॏ DEVANAGARI VOWEL SIGN AW • Kashmiri Sign
+
+        // Dependent vowel sign
+        //""    :   "\u0955", // $ॕ DEVANAGARI VOWEL SIGN CANDRA LONG E • used in transliteration of Avestan Dependent vowel signs for Kashmiri
+        //""    :   "\u0956", // $ॖ DEVANAGARI VOWEL SIGN UE
+        //""    :   "\u0957", // $ॗ DEVANAGARI VOWEL SIGN UUE
+
+        "L^i"   :   "\u0962", // ॢ DEVANAGARI VOWEL SIGN VOCALIC L
+        "LLi"   :   "\u0962", // ॢ DEVANAGARI VOWEL SIGN VOCALIC L
+        "L^I"   :   "\u0963", // ॣ DEVANAGARI VOWEL SIGN VOCALIC LL
+        "LLI"   :   "\u0963"  // ॣ DEVANAGARI VOWEL SIGN VOCALIC LL
+    },
+
+    "Consonants": { // ()+
+        // Consonants
+        "k"     :   "\u0915", // क DEVANAGARI LETTER KA
+        "kh"    :   "\u0916", // ख DEVANAGARI LETTER KHA
+        "g"     :   "\u0917", // ग DEVANAGARI LETTER GA
+        "gh"    :   "\u0918", // घ DEVANAGARI LETTER GHA
+        "~N"    :   "\u0919", // ङ DEVANAGARI LETTER NGA
+        "N^"    :   "\u0919", // ङ DEVANAGARI LETTER NGA
+        "ch"    :   "\u091A", // च DEVANAGARI LETTER CA
+        "Ch"    :   "\u091B", // छ DEVANAGARI LETTER CHA
+        "chh"   :   "\u091B", // छ DEVANAGARI LETTER CHA
+        "j"     :   "\u091C", // ज DEVANAGARI LETTER JA
+        "jh"    :   "\u091D", // झ DEVANAGARI LETTER JHA
+        "~n"    :   "\u091E", // ञ DEVANAGARI LETTER NYA
+        "n^"    :   "\u091E", // ञ DEVANAGARI LETTER NYA
+        "JN"    :   "\u091E", // ञ DEVANAGARI LETTER NYA
+        "T"     :   "\u091F", // ट DEVANAGARI LETTER TTA
+        "Th"    :   "\u0920", // ठ DEVANAGARI LETTER TTHA
+        "D"     :   "\u0921", // ड DEVANAGARI LETTER DDA
+        "Dh"    :   "\u0922", // ढ DEVANAGARI LETTER DDHA
+        "N"     :   "\u0923", // ण DEVANAGARI LETTER NNA
+        "t"     :   "\u0924", // त DEVANAGARI LETTER TA
+        "th"    :   "\u0925", // थ DEVANAGARI LETTER THA
+        "d"     :   "\u0926", // द DEVANAGARI LETTER DA
+        "dh"    :   "\u0927", // ध DEVANAGARI LETTER DHA
+        "n"     :   "\u0928", // न DEVANAGARI LETTER NA
+        "^n"    :   "\u0929", // ऩ DEVANAGARI LETTER NNNA • for transcribing Dravidian alveolar n ≡ 0928 न  093C $़
+        "p"     :   "\u092A", // प DEVANAGARI LETTER PA
+        "ph"    :   "\u092B", // फ DEVANAGARI LETTER PHA
+        "b"     :   "\u092C", // ब DEVANAGARI LETTER BA
+        "bh"    :   "\u092D", // भ DEVANAGARI LETTER BHA
+        "m"     :   "\u092E", // म DEVANAGARI LETTER MA
+        "y"     :   "\u092F", // य DEVANAGARI LETTER YA
+        "r"     :   "\u0930", // र DEVANAGARI LETTER RA
+        "R"     :   "\u0931", // ऱ DEVANAGARI LETTER RRA • for transcribing Dravidian alveolar r • half form is represented as “Eyelash RA” ≡ 0930 र  093C $़
+        "l"     :   "\u0932", // ल DEVANAGARI LETTER LA
+        "L"     :   "\u0933", // ळ DEVANAGARI LETTER LLA
+        //"ld"  :   "\u0933", // ळ DEVANAGARI LETTER LLA
+        //""    :   "\u0934", // ऴ DEVANAGARI LETTER LLLA • for transcribing Dravidian l ≡ 0933 ळ  093C $़
+        "w"     :   "\u0935", // व DEVANAGARI LETTER VA
+        "v"     :   "\u0935", // व DEVANAGARI LETTER VA
+        "sh"    :   "\u0936", // श DEVANAGARI LETTER SHA
+        "Sh"    :   "\u0937", // ष DEVANAGARI LETTER SSA
+        "shh"   :   "\u0937", // ष DEVANAGARI LETTER SSA
+        "s"     :   "\u0938", // स DEVANAGARI LETTER SA
+        "h"     :   "\u0939", // ह DEVANAGARI LETTER HA
+
+        // Manually Added
+        "x"     :   "\u0915"+"\u094D"+"\u0937", // क्ष == "kSh"
+        "GY"    :   "\u091C"+"\u094D"+"\u091E", // == "j~n"
+
+        // Additional consonants
+        "q"     :   "\u0958", // क़ DEVANAGARI LETTER QA ≡ 0915 क  093C $़
+        "K"     :   "\u0959", // ख़ DEVANAGARI LETTER KHHA ≡ 0916 ख  093C $़
+        "G"     :   "\u095A", // ग़ DEVANAGARI LETTER GHHA ≡ 0917 ग  093C $़
+        "z"     :   "\u095B", // ज़ DEVANAGARI LETTER ZA ≡ 091C ज  093C $़
+        "J"     :   "\u095B", // ज़ DEVANAGARI LETTER ZA ≡ 091C ज  093C $़
+        ".D"    :   "\u095C", // ड़ DEVANAGARI LETTER DDDHA ≡ 0921 ड  093C $़
+        ".Dh"   :   "\u095D", // ढ़ DEVANAGARI LETTER RHA ≡ 0922 ढ  093C $़
+        "f"     :   "\u095E"  // फ़ DEVANAGARI LETTER FA ≡ 092B फ  093C $़
+        //""    :   "\u095F"  // य़ DEVANAGARI LETTER YYA ≡ 092F य  093C $़
+    },
+
+    "Accent_marks": {
+        "\u200B":   "\u200B", // ZERO WIDTH SPACE (ZWSP)
+        "\u200D":   "\u200D", // ZERO WIDTH JOINER (ZWJ)
+        // Hack to handle accent marks, if used in Indian scripts or if language specific accent marks does not exist
+        "\u0300":   "\u0300", // $॓ GRAVE ACCENT → 0300 $̀   combining grave accent
+        "\u0301":   "\u0301", // $॔ ACUTE ACCENT → 0301 $́   combining acute accent
+        "\u0307":   "\u0307", // ॱ SIGN HIGH SPACING DOT
+        "\u030A":   "\u030A", // ॰ ABBREVIATION SIGN • intended for Devanagari-specific abbreviations
+        "\u030D":   "\u030D", // $॑ STRESS SIGN UDATTA = Vedic tone svarita
+        "\u0323":   "\u0323", // $़ SIGN NUKTA • for extending the alphabet to new letters
+        "\u0332":   "\u0332", // $॒ STRESS SIGN ANUDATTA = Vedic tone anudatta
+
+        // Various signs
+        "\u0323":   "\u093C", // $़ DEVANAGARI SIGN NUKTA • for extending the alphabet to new letters
+
+        // Vedic tone marks
+        "\u030D":   "\u0951", // $॑ DEVANAGARI STRESS SIGN UDATTA = Vedic tone svarita • mostly used for svarita, with rare use for udatta • used also in Vedic texts written in other scripts → 1CDA $᳚   vedic tone double svarita
+        "\u0332":   "\u0952", // $॒ DEVANAGARI STRESS SIGN ANUDATTA = Vedic tone anudatta • used also in Vedic texts written in other scripts → 1CDC $᳜   vedic tone kathaka anudatta
+
+        // Accent marks
+        "\u0300":   "\u0953", // $॓ DEVANAGARI GRAVE ACCENT → 0300 $̀   combining grave accent
+        "\u0301":   "\u0954", // $॔ DEVANAGARI ACUTE ACCENT → 0301 $́   combining acute accent
+
+        // Abbreviation sign
+        "\u030A":   "\u0970", // ॰ DEVANAGARI ABBREVIATION SIGN • intended for Devanagari-specific abbreviations
+        "\u0307":   "\u0971"  // ॱ DEVANAGARI SIGN HIGH SPACING DOT
+    },
+    
+    "Others": { // ()
+        // Various signs
+        //""    :   "\u0900", // $ऀ DEVANAGARI SIGN INVERTED CANDRABINDU = vaidika adhomukha candrabindu
+        ".N"    :   "\u0901", // $ँ DEVANAGARI SIGN CANDRABINDU = anunasika → 0310 $̐   combining candrabindu
+        ".m"    :   "\u0902", // $ं DEVANAGARI SIGN ANUSVARA = bindu
+        ".n"    :   "\u0902", // $ं DEVANAGARI SIGN ANUSVARA = bindu
+        "M"     :   "\u0902", // $ं DEVANAGARI SIGN ANUSVARA = bindu
+        "H"     :   "\u0903", // $ः DEVANAGARI SIGN VISARGA
+        ".a"    :   "\u093D", // ऽ DEVANAGARI SIGN AVAGRAHA
+        ".h"    :   "\u094D", // $ DEVANAGARI SIGN VIRAMA = halant (the preferred Hindi name) • suppresses inherent vowel
+
+        // copied from Dependent vowels, for handling individual charecters
+        ".c"    :   "\u0945", // $ॅ DEVANAGARI VOWEL SIGN CANDRA E = candra ** Manual Change ***
+
+        // Digits
+        "0"     :   "\u0966", // ० DEVANAGARI DIGIT ZERO
+        "1"     :   "\u0967", // १  DEVANAGARI DIGIT ONE
+        "2"     :   "\u0968", // २ DEVANAGARI DIGIT TWO
+        "3"     :   "\u0969", // ३ DEVANAGARI DIGIT THREE
+        "4"     :   "\u096A", // ४ DEVANAGARI DIGIT FOUR
+        "5"     :   "\u096B", // ५ DEVANAGARI DIGIT FIVE
+        "6"     :   "\u096C", // ६ DEVANAGARI DIGIT SIX
+        "7"     :   "\u096D", // ७ DEVANAGARI DIGIT SEVEN
+        "8"     :   "\u096E", // ८ DEVANAGARI DIGIT EIGHT
+        "9"     :   "\u096F", // ९ DEVANAGARI DIGIT NINE
+
+        // Additional consonants
+        //""    :   "\u0979", // ॹ DEVANAGARI LETTER ZHA • used in transliteration of Avestan
+        //""    :   "\u097A", // ॺ DEVANAGARI LETTER HEAVY YA • used for an affricated glide JJYA Sindhi implosives
+        //""    :   "\u097B", // ॻ DEVANAGARI LETTER GGA
+        //""    :   "\u097C", // ॼ DEVANAGARI LETTER JJA Glottal stop
+        //""    :   "\u097D", // ॽ DEVANAGARI LETTER GLOTTAL STOP • used for writing Limbu in Devanagari • a glyph variant has the connecting top bar Sindhi implosives
+        //""    :   "\u097E", // ॾ DEVANAGARI LETTER DDDA
+        //""    :   "\u097F", // ॿ DEVANAGARI LETTER BBA
+
+        //Generic punctuation for scripts of India
+        ","     :   "\u0964", // । DEVANAGARI DANDA = purna viram • phrase separator
+        "."     :   "\u0965"  // ॥ DEVANAGARI DOUBLE DANDA = deergh viram
+    },
+
+    // Virama
+    "VIRAMA"    :   "\u094D"  // $ DEVANAGARI SIGN VIRAMA = halant (the preferred Hindi name) • suppresses inherent vowel
+};
